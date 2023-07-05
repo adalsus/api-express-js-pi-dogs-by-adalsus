fc60fd4b90937ee757e58225ca09cfa129d99359
adalsus <adalbertomonar@hotmail.com>
2023-07-05 17:13:31 -0500

    adaptaciones temporales para que haga deploy en sitio web glitch.com

--- src/sequelize/db.js
+++ src/sequelize/db.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 const { fdeco } = require('../fns/fnsApi.js');
 
 const { DB_USER,DB_PASSWORD,DB_HOST,DB_PORT,DB_NAME,API_KEY,EP_BREEDS_TDA,URI_CLOUD } = process.env;
-//const _DB_USER = fdeco(JSON.parse(DB_USER))
-//const _DB_PASSWORD = fdeco(JSON.parse(DB_PASSWORD))
-//const _API_KEY = fdeco(JSON.parse(API_KEY))
 const _URI_CLOUD = fdeco(JSON.parse(URI_CLOUD))
 
-const eresAdalbertoMonar = "SI"
+const eresAdalbertoMonar = "SI"//<-- Cambia sólo aquí a NO en caso de ser otro Programmer 
 const PostgreSQL_LOCAL = "NO"
-const postgresql_uri = ( eresAdalbertoMonar==='SI' )
+
+if (eresAdalbertoMonar==='SI' && PostgreSQL_LOCAL==='SI') {
+  const _DB_USER = fdeco(JSON.parse(DB_USER))
+  const _DB_PASSWORD = fdeco(JSON.parse(DB_PASSWORD))
+  const _API_KEY = fdeco(JSON.parse(API_KEY))
+}
+
+const postgresql_uri = (eresAdalbertoMonar==='SI')
 ?(PostgreSQL_LOCAL==='SI')
   ?`postgres://${_DB_USER}:${_DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
   :`postgres://${_URI_CLOUD}`
