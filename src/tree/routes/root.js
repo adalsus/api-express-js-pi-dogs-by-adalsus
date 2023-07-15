d4738af9d50299ee25414430bcbe56347b805820
adalsus <adalbertomonar@hotmail.com>
2023-07-15 09:18:15 -0500

    Añadí información de como usarla esta API server

--- src/tree/routes/root.js
+++ src/tree/routes/root.js
@@ -9,7 +9,14 @@ endpoint.get("/", (req, res) => {
             'Dogs Model is already coincident!<br>'+
             'Temperaments Model is already coincident!<br>'+
             'The model for the Temperament Set is already coincident!<br>'+
-            'Associations already in place!!!<br>');
+            'Associations already in place!!!<br>'+
+            '<br><br>'+
+            'PETICIONES O ENLACES DISPONIBLES QUE PUEDE USAR:<br>'+
+            '/  --Es justo en el que está ahorita, está en la raiz de la API<br>'+
+            '/dogs  --Añadiendo solo esto al final de la dirección web, responderá con todas las razas.json almacenadas en la base de datos<br>'+
+            '/dogs?name=toy  --Añadiendo solo esto al final de la dirección web, responderá con la(s) raza(s).json que en su name(nombre) tenga(n) toy<br>'+
+            '/dogs/100  --Añadiendo solo esto al final de la dirección web, responderá con la raza.json cuyo id(código identificador) sea el 100<br>'+
+            '/temperaments  --Añadiendo solo esto al final de la dirección web, responderá con todos los temperaments.json(temperamentos) recopilados.');
 });
 
 
